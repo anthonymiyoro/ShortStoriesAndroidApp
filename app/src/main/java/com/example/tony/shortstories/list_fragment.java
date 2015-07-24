539f98856a841708894c0bc6ec54d20f14155f35
anthonymiyoro <amiyoro@gmail.com>
2015-07-24 12:27:38 +0300

    Added stories list to fragment

--- app/src/main/java/com/example/tony/shortstories/list_fragment.java
+++ app/src/main/java/com/example/tony/shortstories/list_fragment.java
@@ -1,7 +1,19 @@
 package com.example.tony.shortstories;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by tony on 24/07/15.
  */
-public class list_fragment {
+public class list_fragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.stories_list_fragment,container,false);
+    }
 }
